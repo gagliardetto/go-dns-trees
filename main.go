9492072bfe4076ad123e50d99e53903bd421d445
Slavomir <gagliardetto@users.noreply.github.com>
2019-05-12 22:31:03 +0300

    percorri tutti i percorsi (credo; forse il check elimina quello che non dovrebbe)

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"math/rand"
+	"net"
 	"os"
+	"sort"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/davecgh/go-spew/spew"
@@ -20,13 +24,13 @@ var rootNS = getRandomRootDNS()
 
 func main() {
 	targets := []string{
+		"ticonsultores.biz.ni",
 		"adblock-data.brave.com",
 		"mail.google.com",
 		"example.com",
 		"ledger.brave.com",
-		"ticonsultores.biz.ni",
 	}
-	generateGraphsFor(targets[0])
+	generateGraphsFor(targets[4])
 }
 
 func generateGraphsFor(targets ...string) {
@@ -74,6 +78,28 @@ func generateGraphFor(target string) error {
 	return nil
 }
 
+var (
+	registry = map[string]bool{}
+	mu       *sync.RWMutex
+)
+
+func init() {
+	mu = &sync.RWMutex{}
+}
+
+func Has(id string) bool {
+	mu.RLock()
+	defer mu.RUnlock()
+	_, has := registry[id]
+	return has
+}
+
+func Add(id string) {
+	mu.Lock()
+	defer mu.Unlock()
+	registry[id] = true
+}
+
 func goExplore(
 	g *dot.Graph,
 	parentNode dot.Node,
@@ -81,138 +107,150 @@ func goExplore(
 	target string,
 	queryType uint16,
 ) {
-	for {
-		debugf(
-			"query: authority:%q, target:%q, queryType:%q\n",
-			authority,
-			target,
-			dns.TypeToString[queryType],
-		)
-		res, err := Send(authority, target, queryType)
-		if err != nil {
-			if isErrNoSuchHost(err) {
-				debug(authority, ": no such host")
-				addErrorNode(g, parentNode, authority, "no such host")
-				break
-			} else {
-				panic(err)
-			}
-		}
+	debugf(
+		"query: authority:%q, target:%q, queryType:%q\n",
+		authority,
+		target,
+		dns.TypeToString[queryType],
+	)
 
-		hasNextAuthority := len(res.Ns) > 0
-		debugf(
-			"answer: isAuthoritative:%v, hasNextAuthority:%v\n",
-			res.Authoritative,
-			hasNextAuthority,
-		)
-		if hasNextAuthority {
-			// given that we expect this call to get a non-authoritative answer,
-			// let's extract the RR contained in the authority section:
-			nextAuthorities := extractAllNS(res.Ns)
-
-			// let's randomly choose the NS (from the authority list) for the next
-			// query:
-			randomNS := chooseRandomNS(nextAuthorities)
-			authority = randomNS.Ns
-
-			// link all NS to parent:
-			for _, auth := range nextAuthorities {
-				authorityNode := g.Node(auth.Ns)
-
-				style := "dashed"
-				//let's make it bold if the NS is gonna be used for the
-				// next query:
-				if auth.Ns == authority {
-					style = "bold"
-				}
-				g.Edge(
-					parentNode,
-					authorityNode,
-					rcodeLabel(target, res.MsgHdr.Rcode),
-				).
-					Attr("arrowhead", "vee").
-					Attr("arrowtail", "inv").
-					Attr("arrowsize", ".7").
-					//
-					Attr("fontname", "bold").
-					Attr("fontsize", "7.0").
-					Attr("style", style).
-					Attr("fontcolor", RcodeToColor[res.MsgHdr.Rcode])
-			}
-		}
-
-		noSuggestedNextAuthorities := hasNextAuthority == false
-		if !res.Authoritative && noSuggestedNextAuthorities {
-			debug(authority, "is not authoritative, but does not suggest who could be")
-			// dead end:
-			addErrorNode(g, parentNode, "DEAD END", authority+"is not authoritative, but does not suggest who could be")
-			debug(spew.Sdump(res.Answer))
-			// TODO: show eventual A or AAAA or CNAME records (i.e. non-authoritative answers)
-			break
-		}
-
-		parentNode = g.Node(authority)
-		if !res.Authoritative {
-			debug(authority, "is not authoritative")
-			continue
+	// send the query and get the response:
+	res, err := Send(authority, target, queryType)
+	if err != nil {
+		if isErrNoSuchHost(err) {
+			debugf("	%q: no such host\n", authority)
+			addErrorNode(g, parentNode, authority, "no such host")
+			return
 		} else {
-			debug(authority, "is authoritative")
-			parentNode.
-				Attr("style", "filled").
-				Attr("fillcolor", "#0099ff")
+			panic(err)
+		}
+	}
 
-			var content string
+	hasNextAuthority := len(res.Ns) > 0
+	debugf(
+		"	answer: isAuthoritative:%v, hasNextAuthority:%v\n",
+		res.Authoritative,
+		hasNextAuthority,
+	)
+	if !res.Authoritative && hasNextAuthority {
+		// given that we expect this call to get a non-authoritative answer,
+		// let's extract the RR contained in the authority section:
+		nextAuthorities := extractNS(res.Ns)
 
-			for _, ans := range res.Answer {
-				switch answerElem := ans.(type) {
-				case *dns.A:
-					{
-						content += fmt.Sprintf(
-							"[A]%v\n",
-							answerElem.A,
-						)
-					}
-				case *dns.CNAME:
-					{
-						content += fmt.Sprintf(
-							"[CNAME]%v\n",
-							answerElem.Target,
-						)
-					}
-				case *dns.AAAA:
-					{
-						content += fmt.Sprintf(
-							"[AAAA]%v\n",
-							answerElem.AAAA,
-						)
-					}
-				}
+		for authIndex := range nextAuthorities {
+			style := "dashed"
+			auth := nextAuthorities[authIndex]
+			//let's make it bold if the NS is gonna be used for the
+			// next query:
+			authorityNode := g.Node(auth.Ns)
 
-			}
-			resultNode := g.Node(content).
-				Attr("style", "filled")
 			g.Edge(
 				parentNode,
-				resultNode,
+				authorityNode,
 				rcodeLabel(target, res.MsgHdr.Rcode),
 			).
 				Attr("arrowhead", "vee").
 				Attr("arrowtail", "inv").
 				Attr("arrowsize", ".7").
-				Attr("color", "#0099ff").
 				//
 				Attr("fontname", "bold").
 				Attr("fontsize", "7.0").
-				Attr("style", "bold").
+				Attr("style", style).
 				Attr("fontcolor", RcodeToColor[res.MsgHdr.Rcode])
-			if cname, ok := hasCNAME(res.Answer); ok {
-				debug("cname:", cname.Target)
-				target = cname.Target
-				authority = rootNS
-				parentNode = resultNode
-			} else {
-				break
+
+			id := fmt.Sprintf("%v:%v", auth.Ns, target)
+
+			if !Has(id) {
+				Add(id)
+				goExplore(g, authorityNode, auth.Ns, target, queryType)
 			}
+		}
+
+		return
+	}
+
+	noSuggestedNextAuthorities := hasNextAuthority == false
+	if !res.Authoritative && noSuggestedNextAuthorities {
+		debug(authority, "is not authoritative, but does not suggest who could be")
+		// dead end:
+		addErrorNode(g, parentNode, "DEAD END", authority+"is not authoritative, but does not suggest who could be next")
+		debug(spew.Sdump(res.Answer))
+		// TODO: show eventual A or AAAA or CNAME records (i.e. non-authoritative answers)
+		return
+	}
+
+	if res.Authoritative {
+		parentNode.
+			Attr("style", "filled").
+			Attr("fillcolor", "#0099ff")
+
+		var content string
+
+		Arecords := extractA(res.Answer)
+		AAAArecords := extractAAAA(res.Answer)
+		CNAMErecords := extractCNAME(res.Answer)
+
+		adder := func(label string, val interface{}) string {
+			return fmt.Sprintf(
+				"[%s]%v\n",
+				label,
+				val,
+			)
+		}
+
+		for _, v := range Arecords {
+			content += adder("A", v.A)
+		}
+		for _, v := range AAAArecords {
+			content += adder("AAAA", v.AAAA)
+		}
+		for _, v := range CNAMErecords {
+			content += adder("CNAME", v.Target)
+		}
+
+		resultNode := g.Node(content).
+			Attr("style", "filled")
+
+		if len(CNAMErecords) == 0 {
+			resultNode.
+				Attr("fillcolor", "#00FF1F")
+		}
+
+		g.Edge(
+			parentNode,
+			resultNode,
+			rcodeLabel(target, res.MsgHdr.Rcode),
+		).
+			Attr("arrowhead", "vee").
+			Attr("arrowtail", "inv").
+			Attr("arrowsize", ".7").
+			Attr("color", "#0099ff").
+			//
+			Attr("fontname", "bold").
+			Attr("fontsize", "7.0").
+			Attr("style", "bold").
+			Attr("fontcolor", RcodeToColor[res.MsgHdr.Rcode])
+
+		if cname, ok := hasCNAME(res.Answer); ok {
+			debug("	cname:", cname.Target)
+
+			exists, err := DomainExists(cname.Target)
+			if err != nil {
+				panic(err)
+			}
+			if exists {
+				resultNode.
+					Attr("fillcolor", "#00FF1F")
+			} else {
+				resultNode.
+					Attr("fillcolor", "#FF1000")
+			}
+
+			target = cname.Target
+			authority = rootNS
+			parentNode = resultNode
+
+			goExplore(g, parentNode, authority, target, queryType)
 		}
 	}
 }
@@ -246,9 +284,14 @@ func isErrNoSuchHost(e error) bool {
 func stripFinalDot(s string) string {
 	return strings.TrimSuffix(s, ".")
 }
+
+var (
+	dnsClient = new(dns.Client)
+)
+
 func Send(server string, domain string, t uint16) (*dns.Msg, error) {
 	m := new(dns.Msg)
-	c := new(dns.Client)
+
 	m.SetQuestion(dns.Fqdn(domain), t)
 
 	// VERY IMPORTANT:
@@ -256,7 +299,7 @@ func Send(server string, domain string, t uint16) (*dns.Msg, error) {
 	// NOTE: does not work in Italy (via VPN); the result returns directly the A and CNAME records even if asking the root servers.
 	// NOTE: works in NL (via VPN).
 
-	r, _, err := c.Exchange(m, stripFinalDot(server)+":"+"53")
+	r, _, err := dnsClient.Exchange(m, stripFinalDot(server)+":"+"53")
 	if err != nil {
 		return nil, fmt.Errorf("error while exchanging DNS message: %s", err)
 	}
@@ -285,13 +328,17 @@ func getRandomRootDNS() string {
 	return rootServers[randomInt(0, len(rootServers)-1)].(*dns.NS).Ns
 }
 
-func extractAllNS(nsRR []dns.RR) []*dns.NS {
+func extractNS(nsRR []dns.RR) []*dns.NS {
 	var ns []*dns.NS
 	for _, a := range nsRR {
 		if _, ok := a.(*dns.NS); ok {
 			ns = append(ns, a.(*dns.NS))
 		}
 	}
+
+	sort.Slice(ns, func(i, j int) bool {
+		return ns[i].Ns < ns[j].Ns
+	})
 	return ns
 }
 func chooseRandomNS(ns []*dns.NS) *dns.NS {
@@ -310,6 +357,62 @@ func hasCNAME(rr []dns.RR) (*dns.CNAME, bool) {
 		}
 	}
 	return cname, cname != nil
+}
+
+func extractA(rr []dns.RR) []*dns.A {
+	var Arecords []*dns.A
+	for i := range rr {
+		a := rr[i]
+		if _, ok := a.(*dns.A); ok {
+			Arecords = append(Arecords, a.(*dns.A))
+		}
+	}
+
+	sort.Slice(Arecords, func(i, j int) bool {
+		return bytes.Compare(Arecords[i].A, Arecords[j].A) < 0
+	})
+	return Arecords
+}
+func extractAAAA(rr []dns.RR) []*dns.AAAA {
+	var AAAArecords []*dns.AAAA
+	for i := range rr {
+		a := rr[i]
+		if _, ok := a.(*dns.AAAA); ok {
+			AAAArecords = append(AAAArecords, a.(*dns.AAAA))
+		}
+	}
+
+	sort.Slice(AAAArecords, func(i, j int) bool {
+		return bytes.Compare(AAAArecords[i].AAAA, AAAArecords[j].AAAA) < 0
+	})
+	return AAAArecords
+}
+func extractCNAME(rr []dns.RR) []*dns.CNAME {
+	var cname []*dns.CNAME
+	for i := range rr {
+		a := rr[i]
+		if _, ok := a.(*dns.CNAME); ok {
+			cname = append(cname, a.(*dns.CNAME))
+		}
+	}
+	return cname
+}
+
+//  DomainExists tries to resolve the domains.
+func DomainExists(domain string) (bool, error) {
+
+	ips, err := net.LookupHost(domain)
+	if err != nil {
+		if isErrNoSuchHost(err) {
+			return false, nil
+		}
+		return false, err
+	}
+	if len(ips) == 0 {
+		return false, nil
+	}
+
+	return true, nil
 }
 
 // RcodeToColor maps Rcodes to colors.
